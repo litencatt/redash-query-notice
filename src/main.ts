40f5f30813f2cdda5da6974105cfc43cc03dd986
litencatt <ncl0709@gmail.com>
2020-08-22 20:08:08 +0900

    Change to static

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,30 @@
+import { Config } from "./config";
 import { Redash } from "./redash";
 import { Scheduler } from "./scheduler";
 import { Slack } from "./slack";
 
 const ps = PropertiesService.getScriptProperties();
-const sheetId = ps.getProperty("SHEET_ID");
-const sheetUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/edit`;
-
-const redashUrl = ps.getProperty("REDASH_URL");
-const redashToken = ps.getProperty("REDASH_USER_TOKEN");
-
-const slackUrl = ps.getProperty("SLACK_INCOMING_WEBHOOK_URL");
-const footer = "<https://github.com/litencatt/redash-query-notice|Redash query notice>";
+Config.sheetId = ps.getProperty("SHEET_ID");
+Redash.redashUrl = ps.getProperty("REDASH_URL");
+Redash.redashToken = ps.getProperty("REDASH_USER_TOKEN");
+Slack.notifyUrl = ps.getProperty("SLACK_INCOMING_WEBHOOK_URL");
+Slack.sheetUrl = `https://docs.google.com/spreadsheets/d/${Config.sheetId}/edit`;
+Slack.footer = "<https://github.com/litencatt/redash-query-notice|Redash query notice>";
 
 function notify() {
-  // Setup to read columns from spread sheat
-  const s = SpreadsheetApp.openById(sheetId);
-  const sheet = s.getSheetByName("config");
-  const startRow = 2;
-  const startColumn = 1;
-  const numRows = sheet.getLastRow();
-  const numColumns = sheet.getLastColumn();
-  const data = sheet.getSheetValues(startRow, startColumn, numRows - 1, numColumns);
-
-  // Column number assigned to the task
-  const enabledColumn = 0;
-  const execAtColumn = 1;
-  const srcServiceColumn = 2;
-
   const now = new Date();
-  const redash = new Redash(redashUrl, redashToken);
 
-  for (const task of data) {
-    const enabled = task[enabledColumn];
-    const execAt = task[execAtColumn];
+  for (const task of Config.getTasks()) {
+    const enabled = task[Config.enabledColumn];
+    const execAt = task[Config.execAtColumn];
     if (!Scheduler.isExecute(now, enabled, execAt)) {
       continue;
     }
 
     let res = null;
-    switch (task[srcServiceColumn]) {
+    switch (task[Config.srcServiceColumn]) {
       case "redash":
-        res = redash.run(task);
+        res = Redash.run(task);
         break;
       default:
         return;
--- src/slack.ts
+++ src/slack.ts
@@ -1,6 +1,22 @@
 export class Slack {
-    public static postMessaage(notifyUrl: string, payload: any) {
-        const res = UrlFetchApp.fetch(notifyUrl, {
+    public static sheetUrl: string;
+    public static notifyUrl: string;
+    public static footer: string;
+
+    public static postMessaage(r: Result) {
+        const payload = {
+            attachments: [
+              {
+                color: "good",
+                title: r.title,
+                title_link: this.sheetUrl,
+                fields: r.fields,
+                footer: this.footer,
+              },
+            ],
+          };
+
+        const res = UrlFetchApp.fetch(this.notifyUrl, {
             method: "post",
             contentType: "application/json",
             payload: JSON.stringify(payload),
